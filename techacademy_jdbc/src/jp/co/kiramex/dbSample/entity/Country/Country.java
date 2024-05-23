1dfa5c7558a14517b11233b891d33008ec080e2f
sy-hiro <oneok.yukki127@gmail.com>
2024-05-24 01:14:31 +0900

    ADD:DAO,DTO

---
+++ techacademy_jdbc/src/jp/co/kiramex/dbSample/entity/Country/Country.java
@@ -0,0 +1,34 @@
+package jp.co.kiramex.dbSample.entity.Country;
+
+public class Country {
+	// フィールド
+	private String name;
+	private int population;
+	
+	// 引数なしコンストラクタ
+	public Country() {
+	}
+	
+	// 引数ありコンストラクタ
+	public Country(String name, int population) {
+		this.name = name;
+		this.population = population;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+
+}

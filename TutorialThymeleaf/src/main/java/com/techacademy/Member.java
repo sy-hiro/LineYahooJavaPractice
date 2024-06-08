db9575187de6ae14d0a72627b9d743ef6d92b44d
sy-hiro <oneok.yukki127@gmail.com>
2024-06-08 17:41:28 +0900

    ADD:Thymeleaf

---
+++ TutorialThymeleaf/src/main/java/com/techacademy/Member.java
@@ -0,0 +1,14 @@
+package com.techacademy;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Member {
+	private int id;
+	private String name;
+	private String gender;
+	private int age;
+
+}
